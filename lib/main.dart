25f1e96cb06e3eefc33b0aafce79fd045883151f
Everton <ehokeverton@gmail.com>
2020-02-09 19:27:13 -0400

    Add Success widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alcoolougasolina/widgets/input.widget.dart';
 import 'package:alcoolougasolina/widgets/loading-button.widget.dart';
 import 'package:alcoolougasolina/widgets/logo.widget.dart';
+import 'package:alcoolougasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -32,37 +33,9 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compensa utilizar álcool",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display",
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false,
-                  invert: true,
-                  func: () {},
-                  text: 'CALCULAR NOVAMENTE'
-                ),
-              ],
-            ),
+          SuccessSession(
+            reset: (){},
+            result: 'Compensa utilizar X',
           ),
           Input(
             controller: _gasCtrl,
